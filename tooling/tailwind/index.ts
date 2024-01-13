54d9edb2702d90f5b3f9e9b8adc56740db6ddcce
Matheus Sá <matheus.souza410@etec.sp.gov.br>
2024-01-13 15:31:08 -0300

    BigBang

---
+++ tooling/tailwind/index.ts
@@ -0,0 +1,8 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: ["src/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+  theme: {},
+  plugins: [],
+} satisfies Config;
